897d929ac234ddff029272eb390097c3124e1a84
Charles Waddell <charles@silverorange.com>
2016-04-21 11:37:09 -0300

    Switch between local and composer packages task

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 
-var symlinks = require('./lib/symlinks');
+var vendor_symlinks = require('./lib/vendor-symlinks');
+var package_symlinks = require('./lib/package-symlinks');
 var concentrate = require('./lib/concentrate');
 var paths = require('./lib/paths');
 var flags = require('./lib/flags');
@@ -12,12 +13,14 @@ var phplint = require('./lib/phplint');
 var less = require('./lib/less');
 
 gulp.task('phplint', phplint.task);
-gulp.task('setup-symlinks', symlinks.task.setup);
-gulp.task('teardown-symlinks', symlinks.task.teardown);
+gulp.task('setup-vendor-symlinks', vendor_symlinks.task.setup);
+gulp.task('teardown-vendor-symlinks', vendor_symlinks.task.teardown);
+gulp.task('setup-package-symlinks', package_symlinks.task.setup);
+gulp.task('teardown-package-symlinks', package_symlinks.task.teardown);
 gulp.task('clean', [ 'teardown-symlinks' ], clean.task);
 gulp.task('concentrate-internal', [ 'setup-symlinks' ], concentrate.task);
 gulp.task('concentrate', [ 'concentrate-internal' ], function() {
-	return symlinks.task.teardown();
+	return package_symlinks.task.teardown();
 });
 gulp.task('build-less', [ 'write-flag' ], less.task);
 gulp.task('write-flag', flags.task);
@@ -26,7 +29,8 @@ function cleanShutdown() {
 	gutil.log(gutil.colors.blue('BYE'));
 
 	flags.remove();
-	symlinks.task.teardown();
+	vendor_symlinks.task.teardown();
+	package_symlinks.task.teardown();
 
 	process.exit();
 }
@@ -35,7 +39,7 @@ function cleanShutdown() {
  * Watches LESS and JS files for changes and recompiles/minifies/bundles
  * them.
  */
-gulp.task('watch', [ 'setup-symlinks' ], function () {
+gulp.task('watch', [ 'setup-package-symlinks', 'setup-vendor-symlinks' ], function () {
 	process.on('SIGINT', cleanShutdown);
 	process.on('SIGHUP', cleanShutdown);
 	process.on('SIGTERM', cleanShutdown);
